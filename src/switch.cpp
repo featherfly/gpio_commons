d06b16491176aaa002b442b403c9307988d317e5
zhongji <featherfly@foxmail.com>
2020-10-30 14:28:08 +0800

    change switch open close to on off

--- src/switch.cpp
+++ src/switch.cpp
@@ -4,11 +4,11 @@
 
 Switch::Switch(Pin *_pin) : Switch(_pin, true){};
 
-Switch::Switch(Pin *_pin, bool _closeState)
+Switch::Switch(Pin *_pin, bool _offState)
 {
     pin = _pin;
-    closeState = _closeState;
-    Log.notice(F("switch pin %d, closeState %T," CR), pin->pin(), closeState);
+    offState = _offState;
+    Log.notice(F("switch pin %d, offState %T," CR), pin->pin(), offState);
 };
 int Switch::state()
 {
@@ -18,23 +18,23 @@ int Switch::state(bool state)
 {
     return pin->write(state);
 };
-bool Switch::isClose()
+bool Switch::is_off()
 {
     int s = state();
-    return closeState ? s == HIGH : s == LOW;
+    return offState ? s == HIGH : s == LOW;
 };
-bool Switch::isOpen()
+bool Switch::is_on()
 {
     int s = state();
-    return closeState ? s == LOW : s == HIGH;
+    return offState ? s == LOW : s == HIGH;
 };
 
-bool Switch::open()
+bool Switch::turn_on()
 {
-    return pin->write(!closeState);
+    return pin->write(!offState);
 };
 
-bool Switch::close()
+bool Switch::turn_off()
 {
-    return pin->write(closeState);
+    return pin->write(offState);
 };
