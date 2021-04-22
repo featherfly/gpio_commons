82623c0eaa17ac2f56fda62efe1f78a152702962
zhongji <featherfly@foxmail.com>
2021-04-22 14:46:17 +0800

    add lock remove mcuid

---
+++ src/lock.h
@@ -0,0 +1,37 @@
+#ifndef LOCK_H
+#define LOCK_H
+
+#include <switch.h>
+#include <pin.h>
+
+class Lock{
+    private:
+        int _lockPin;
+        int _statePin;
+        bool _writeOffState = false;
+        bool _readOffState = false;
+
+    public:
+        Lock(int lockPin, int statePin);
+        Lock(int lockPin, int statePin, bool offState);
+        Lock(int lockPin, bool writeOffState, int statePin, bool readOffState);
+        // void setSwitch(Switch *lockSwitch);
+        // void setStatePin(Pin *state);
+        void setup(int lockPin, bool writeOffState, int statePin, bool readOffState);
+
+        bool turn(bool state);
+        
+        bool turn_on();
+        
+        bool turn_off();
+
+        bool is_on();
+
+        bool is_off();
+
+        int state();
+
+        
+};
+
+#endif
--- src/mcuid.cpp
+++ src/mcuid.cpp
@@ -1,45 +1,45 @@
-#include "mcuid.h"
+// #include "mcuid.h"
 
-#include <Arduino.h>
-#include <EEPROM.h>
+// #include <Arduino.h>
+// #include <EEPROM.h>
 
-Mcuid::Mcuid(int seed) : Mcuid(seed, 255) {};
-Mcuid::Mcuid(int seed, int defaultValue)
-{
-    _defaultValue = defaultValue;
-    if (EEPROM.read(START_ADDRESS) == START_CHAR && EEPROM.read(END_ADDRESS) == END_CHAR)
-    {
-        _init = true;
-    }
+// Mcuid::Mcuid(int seed) : Mcuid(seed, 255) {};
+// Mcuid::Mcuid(int seed, int defaultValue)
+// {
+//     _defaultValue = defaultValue;
+//     if (EEPROM.read(START_ADDRESS) == START_CHAR && EEPROM.read(END_ADDRESS) == END_CHAR)
+//     {
+//         _init = true;
+//     }
 
-    if (!_init)
-    {
-        randomSeed(seed);
+//     if (!_init)
+//     {
+//         randomSeed(seed);
 
-        EEPROM.write(START_ADDRESS, START_CHAR);
-        for (int address = 1; address < END_ADDRESS; address++)
-        {
-            _id[address - 1] = random_chars[random(0, 36)];
-            EEPROM.write(address, _id[address - 1]);
-        }
-        EEPROM.write(END_ADDRESS, END_CHAR);
-    }
-}
+//         EEPROM.write(START_ADDRESS, START_CHAR);
+//         for (int address = 1; address < END_ADDRESS; address++)
+//         {
+//             _id[address - 1] = random_chars[random(0, 36)];
+//             EEPROM.write(address, _id[address - 1]);
+//         }
+//         EEPROM.write(END_ADDRESS, END_CHAR);
+//     }
+// }
 
-char* Mcuid::id()
-{
-    return _id;
-}
+// char* Mcuid::id()
+// {
+//     return _id;
+// }
 
-int Mcuid::len()
-{
-    return _len;
-}
+// int Mcuid::len()
+// {
+//     return _len;
+// }
 
-void Mcuid::clear()
-{
-    for (int address = 0; address < END_ADDRESS + 1; address++)
-    {
-        EEPROM.write(address, _defaultValue);
-    }
-}
+// void Mcuid::clear()
+// {
+//     for (int address = 0; address < END_ADDRESS + 1; address++)
+//     {
+//         EEPROM.write(address, _defaultValue);
+//     }
+// }
