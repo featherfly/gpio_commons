43b3a47f3c534607560149583dc9a2732e8b0c8b
zhongj <featherfly@foxmail.com>
2023-03-10 17:51:44 +0800

    feat: Pin support INPUT_ANALOG for stm32

--- src/pin.cpp
+++ src/pin.cpp
@@ -1,63 +1,53 @@
+#include "pin.h"
+
 #include <Arduino.h>
 #include <ArduinoLog.h>
 #include <debugs.h>
-#include "pin.h"
 
 Pin::Pin(int pin, int mode) : Pin(pin, mode, false) {}
 
-Pin::Pin(int pin, int mode, bool state)
-{
+Pin::Pin(int pin, int mode, bool state) {
     _pin = pin;
-    _mode = mode,
-    _state = state;
+    _mode = mode, _state = state;
 
     pinMode(_pin, _mode);
 
-    if (mode == OUTPUT) 
-    {
+    if (mode == OUTPUT) {
         write(_state);
     }
     D(Log.infoln(F("pin %d, mode %d, state %d"), _pin, _mode, _state));
 }
 
-int Pin::read()
-{
-    if (_mode == INPUT || _mode == INPUT_PULLUP)
-    {
+int Pin::read() {
+    if (_mode == INPUT || _mode == INPUT_PULLUP) {
         _state = digitalRead(_pin);
         D(Log.traceln(F("Pin[%d].read = %d, mode %d"), _pin, _state, _mode));
         return _state;
-    }
-    else
-    {
+    } else {
         D(Log.traceln(F("Pin[%d].read = %d, mode %d"), _pin, _state, _mode));
         return _state;
     }
 }
 
-int Pin::write(bool state)
-{
+int Pin::write(bool state) {
     D(Log.traceln(F("Pin[%d].write(%d), mode %d"), _pin, _state, _mode));
-    if (_mode == OUTPUT)
-    {
+    if (_mode == OUTPUT) {
         digitalWrite(_pin, state);
         _state = state;
     }
     return _state;
 }
-int Pin::pin()
-{
-    return _pin;
-};
-bool Pin::state()
-{
-    if (_mode == INPUT || _mode == INPUT_PULLUP || _mode == ANALOG)
-    {
+int Pin::pin() { return _pin; };
+bool Pin::state() {
+    if (_mode == INPUT || _mode == INPUT_PULLUP
+#if defined(ANALOG)
+        || _mode == ANALOG
+#elif defined(INPUT_ANALOG)
+        || _mode == INPUT_ANALOG
+#endif
+    ) {
         return read();
-    }    
+    }
     return _state;
 };
-int Pin::mode()
-{
-    return _mode;
-};
+int Pin::mode() { return _mode; };
